d2a149ecb240db40fdaa87c7172d7a783b25afcc
Stefano Mrkt <stefano.mrkt@gmail.com>
2024-07-08 18:34:12 +0200

    Modificato titolo Landing

--- src/components/FreeDesign/HeroFreeDesign/HeroFree.jsx
+++ src/components/FreeDesign/HeroFreeDesign/HeroFree.jsx
@@ -9,16 +9,19 @@ export default function HeroFree() {
   return (
     <div className={style.heroDiv} id="Home">
       <div className={style.heroSx}>
-        <div className={style.myWork}>Lezione gratuita</div>
+        <div className={` ${style.myWork} mulish`}>
+          Sei un Coach o un consulente?
+        </div>
         <div className={` ${style.myName} mulish`}>
-          Scarica la <span className={style.bold}>videolezione gratuita </span>
-          per ottimizzare il tuo sito web attira clienti
+          Scarica la <span className={style.bold}>videolezione gratuita</span>
+          per Coach e Consulenti: Ottimizza il tuo sito web per attirare più
+          clienti
         </div>
         <div className={style.heroDown}>
           <div className={style.line}></div>
-          <p className={style.myDescription}>
-            Impara i segreti del Web Design e scopri come creare un sito Web
-            coinvolgente e strategico.
+          <p className={` ${style.myDescription} mulish`}>
+            Scopri i segreti del Web Design per creare un sito web coinvolgente
+            e strategico, specificamente pensato per coach e consulenti.
           </p>
         </div>
       </div>
